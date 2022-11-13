dae82291c4e5498db5bd465c610dae30a477b698
Iris <agboolaisholaidreez>
2022-11-13 08:53:01 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
-function rotate(nums: number[], k: number): void {
-  for (let i = 0; i < k; i++) {
-    nums.unshift(nums.pop() || nums.length);
-  }
-  console.log(nums);
-}
+// function rotate(nums: number[], k: number): void {
+//   for (let i = 0; i < k; i++) {
+//     const pop = nums.pop();
+//     if (pop !== undefined) nums.unshift(pop);
+//   }
+// }
 
-rotate([1, 2, 3, 4, 5, 6, 7], 3);
+//rotate([2147483647, -2147483648, 33, 219, 0], 4);
