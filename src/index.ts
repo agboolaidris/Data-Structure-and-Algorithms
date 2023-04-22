423545771d6f0246b3fa4a29c959b548686af452
Iris <agboolaisholaidreez>
2023-04-22 16:01:35 +0100

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-/**
- Do not return anything, modify nums in-place instead.
- */
-// function rotate(nums: number[], k: number): void {
-//   for (let i = 0; i < k; i++) {
-//     const pop = nums.pop();
-//     if (pop !== undefined) nums.unshift(pop);
-//   }
-// }
-
-//rotate([2147483647, -2147483648, 33, 219, 0], 4);
+console.log("Hello worlds");
